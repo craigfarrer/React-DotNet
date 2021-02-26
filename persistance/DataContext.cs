6c37d8ab4a7f36a1e7e962d87d84a64ddafd72dc
Craig Farrer <craigfarrer@outlook.com>
2021-02-26 14:27:33 +0000

    End of section 2

---
+++ persistance/DataContext.cs
@@ -0,0 +1,14 @@
+using domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace persistance
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Activity> Activities { get; set; }
+    }
+}
